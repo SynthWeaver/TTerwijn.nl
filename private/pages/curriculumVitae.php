3e20ecdf71c61957b98a8a00155a77a6e04dd634
Tim Terwijn <tim@tterwijn.nl>
2021-10-08 16:43:31 +0200

    Updated my CV

--- private/pages/curriculumVitae.php
+++ private/pages/curriculumVitae.php
@@ -1,6 +1,191 @@
-<main class="home-page">
+<main class="cv-page">
     <h1>Curriculum vitae</h1> 
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <table>
+        <tr>
+            <th colspan="2">Persoonsgegevens</th>
+            <th rowspan="9"><img src="public/img/tim.jpg"></th>
+        </tr>
+        <tr>
+            <td>Naam</td>
+            <td>Tim Terwijn</td>
+        </tr>
+        <tr>
+            <td>Adres</td>
+            <td>Pieterman 12</td>
+        </tr>
+        <tr>
+            <td>Postcode</td>
+            <td>1131 PW Volendam</td>
+        </tr>
+        <tr>
+            <td>Telefoon</td>
+            <td>06-34386374</td>
+        </tr>
+        <tr>
+            <td>Geboortedatum</td>
+            <td>07-09-1995</td>
+        </tr>
+        <tr>
+            <td>E-mail</td>
+            <td>tim@terwijn.nl</td>
+        </tr>
+        <tr>
+            <td colspan="2"><a href="https://linkedin.com/in/tim-t-37611ab6/">LinkedIn</a></td>
+        </tr>
+        <tr>
+            <td colspan="2"><a href="https://github.com/TimTerwijn">Github</a></td>
+        </tr>
+
+        <!-- Opleidingen -->
+        <!-- Todo: openklikbare th's -->
+        
+        <tr>
+            <th colspan="3">Opleidingen</th>
+        </tr>
+        <tr>
+            <td>2017-heden</td>
+            <td colspan="2">HvA - HBO ICT Software Engineering</td>
+        </tr>
+        <tr>
+            <td>2014-2017</td>
+            <td colspan="2">Horizon College - MBO niveau 4 Applicatie Ontwikkelaar Hoorn</td>
+        </tr>
+        <tr>
+            <td>2013-2014</td>
+            <td colspan="2">Horizon College - MBO niveau 3 Verkoopspecialist Hoorn</td>
+        </tr>
+        <tr>
+            <td>2012-2013</td>
+            <td colspan="2">Horizon College - MBO niveau 2 Verkoper Hoorn</td>
+        </tr>
+        <tr>
+            <td>2008-2012</td>
+            <td colspan="2">OSG West Friesland - VMBO TL Hoorn</td>
+        </tr>
+
+        <!-- Minoren -->
+
+        <tr>
+            <th colspan="3">Minoren</th>
+        </tr>
+        <tr>
+            <td>2021</td>
+            <td colspan="2">Minor SEO HvA Business en Economie</td>
+        </tr>
+        <tr>
+            <td>2020</td>
+            <td colspan="2">Minor Web Development HvA CMD</td>
+        </tr>
+        <tr>
+            <td>2019</td>
+            <td colspan="2">Minor Mobile Development HvA ICT</td>
+        </tr>
+
+        <!-- Stages -->
+        
+        <tr>
+            <th colspan="3">Stages</th>
+        </tr>
+        <tr>
+            <td>2020</td>
+            <td colspan="2">Laravel stage OneStep Medemblik</td>
+        </tr>
+        <tr>
+            <td>2017</td>
+            <td colspan="2">Jquery en PHP stage RedButton Amsterdam</td>
+        </tr>
+        <tr>
+            <td>2016</td>
+            <td colspan="2">Django stage Infologic Goorn</td>
+        </tr>
+        <tr>
+            <td>2013-2014</td>
+            <td colspan="2">Verkoopspecialist stage Bouchier Hoorn</td>
+        </tr>
+        <tr>
+            <td>2012-2013</td>
+            <td colspan="2">Verkoper stage Praxis Hoorn</td>
+        </tr>
+
+        <!-- Werkervaring -->
+        
+        <tr>
+            <th colspan="3">Werkervaring</th>
+        </tr>
+        <tr>
+            <td>2019-2020</td>
+            <td colspan="2">Administratief medewerker Deen Hoorn</td>
+        </tr>
+        <tr>
+            <td>2018-2020</td>
+            <td colspan="2">Ochtendkrant bezorgen Berkhout</td>
+        </tr>
+        <tr>
+            <td>2017</td>
+            <td colspan="2">Promotor MonsterScore Amsterdam</td>
+        </tr>
+        <tr>
+            <td>2016</td>
+            <td colspan="2">Administratief medewerker Gunters en Meuser Amsterdam</td>
+        </tr>
+        <tr>
+            <td>2012-2016</td>
+            <td colspan="2">Ochtendkrant bezorgen in Berkhout</td>
+        </tr>
+
+        <!-- Vaardigheden -->
+        <!-- Todo: sorteren -->
+        
+        <tr>
+            <th colspan="3">Vaardigheden</th>
+        </tr>
+        <tr><td colspan="3">GIT</td></tr>
+        <tr><td colspan="3">Scrum</td></tr>
+        <tr><td colspan="3">HTML</td></tr>
+        <tr><td colspan="3">CSS</td></tr>
+        <tr><td colspan="3">SQL</td></tr>
+        <tr><td colspan="3">PHP</td></tr>
+        <tr><td colspan="3">Laravel</td></tr>
+        <tr><td colspan="3">Java</td></tr>
+        <tr><td colspan="3">Spring</td></tr>
+        <tr><td colspan="3">Javascript</td></tr>
+        <tr><td colspan="3">JQuery</td></tr>
+        <tr><td colspan="3">Android</td></tr>
+        <tr><td colspan="3">Bootstrap</td></tr>
+        <tr><td colspan="3">Regular Expressions</td></tr>
+        <tr><td colspan="3">Python</td></tr>
+        <tr><td colspan="3">Vue</td></tr>
+        <tr><td colspan="3">React native</td></tr>
+
+        <!-- Talen -->
+        
+        <tr>
+            <th colspan="3">Talen</th>
+        </tr>
+        <tr>
+            <td>Nederlands</td>
+            <td colspan="2">Moedertaal</td>
+        </tr>
+        <tr>
+            <td>Engels</td>
+            <td colspan="2">Goed</td>
+        </tr>
+
+         <!-- Hobby’s -->
+        
+         <tr>
+            <th colspan="3">Hobby’s</th>
+        </tr>
+        <tr><td>Gamen</td></tr>
+        <tr><td>Computers</td></tr>
+        <tr><td>Lezen</td></tr>
+        <tr><td>Geschiedenis</td></tr>
+        <tr><td>Afspreken met vrienden</td></tr>
+        <tr><td>Uitgaan</td></tr>
+        <tr><td>Zeilen</td></tr>
+        <tr><td>Skateboarden</td></tr>
+        <tr><td>Klimmen</td></tr>
+    </table>    
 </main>
 
 <script>
@@ -8,13 +193,32 @@
 </script>
 
 <style>
-    .home-page>section{
-        display: flex;
-        justify-content:space-between;
-        flex-wrap: wrap;
+    .cv-page{
+
     }
 
-    .home-page>h1{
-        /* text-align: left; */
+    .cv-page>table{
+        text-align: left;
+        margin: 0 auto;
+        margin-bottom: 2rem;
+    }
+
+    .cv-page>table img{
+        display: block;
+        height: 160px;
+        width: 120px;
+        border-radius: 11%;
+    }
+
+    .cv-page>table th{
+        padding: 1rem 0;
+    }
+
+    .cv-page>table>tr :first-child>th{
+        padding-top: 0;
+    }
+
+    .cv-page>table td{
+        padding-right: 1rem;
     }
 </style>
